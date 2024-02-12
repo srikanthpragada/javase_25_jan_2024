7bf2fabbc56791168ab5d597e17a52cb5baa8855
srikanthpragada <srikanthpragada@gmail.com>
2024-02-12 19:56:53 +0530

    12-FEB

--- demo/src/lib1/TestCourse.java
+++ demo/src/lib1/TestCourse.java
@@ -1,33 +1,48 @@
 package lib1;
 
-class Course{
+class Course {
 	private String title;
 	private int fee;
+
 	public Course(String title, int fee) {
 		super();
 		this.title = title;
 		this.fee = fee;
 	}
+	
 	@Override 
+	public int hashCode() {
+		return this.fee;
+	}
+
+	@Override
 	public String toString() {
-		return  this.title + "-" + this.fee;
+		return this.title + "-" + this.fee;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Course) {
+			Course other = (Course) obj;
+			return this.title.equals(other.title) && this.fee == other.fee;
+		} else
+			return false;
 	}
 }
 
-
 public class TestCourse {
-
 	public static void main(String[] args) {
-  
 		var c1 = new Course("Java SE", 6000);
+		System.out.println(c1.hashCode());
 		var c2 = new Course("Java SE", 6000);
+		System.out.println(c2.hashCode());
 		var c3 = new Course("Java EE", 8000);
-		
-		System.out.println(c1);	
-		System.out.println(c1.toString());
-		
-		
 
+		System.out.println(c1);
+		System.out.println(c1.toString());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1.equals(c3));
+		System.out.println(c1.equals("Java SE"));
 	}
 
 }
---
+++ demo/src/lib1/TestPoint.java
@@ -0,0 +1,26 @@
+package lib1;
+
+record Point(int x, int y) {
+	// User-defined method 
+	public void print() {
+		System.out.println(this.x  + ":" + this.y);
+	}
+}
+
+
+public class TestPoint {
+
+	public static void main(String[] args) {
+		var p1 = new Point(10,20);
+		var p2 = new Point(10,20);
+		var p3 = new Point(10,10);
+		
+		System.out.println(p1.equals(p2));
+		System.out.println(p1.x());   // getter method 
+		p1.print(); 
+		
+		
+
+	}
+
+}
